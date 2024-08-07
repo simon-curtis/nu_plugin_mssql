948ef52ec6a56eea191707e5c8c27edf6f22499a
Simon Curtis <simon.curtis@candc-uk.com>
2024-08-07 20:13:05 +0100

    added missing windows target branches

--- src/commands/query.rs
+++ src/commands/query.rs
@@ -135,12 +135,16 @@ async fn handle_err(
                 tiberius::AuthMethod::Integrated => {
                     LabeledError::new("Login failed for integrated auth")
                 }
+                #[cfg(target_os = "windows")]
+                tiberius::AuthMethod::Windows(_) => LabeledError::new("Windows auth not supported yet"),
                 tiberius::AuthMethod::None => LabeledError::new("Login failed for none auth"),
                 tiberius::AuthMethod::SqlServer(_) => LabeledError::new(format!(
                     "Login failed for user {:?}, password: <HIDDEN>",
                     args.user.as_ref().unwrap().as_str(),
                 )),
-                tiberius::AuthMethod::AADToken(_) => panic!("AADToken auth not supported"),
+                tiberius::AuthMethod::AADToken(_) => LabeledError::new("AADToken auth not supported"),
+                #[cfg(target_os = "linux")]
+                _ => todo!(),
             };
             let error = ShellError::LabeledError(Box::new(error));
             Value::error(error, call_head)
